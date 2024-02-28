5fb619f2828160281600958788f94572aff1ed4d
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-28 22:55:37 +0100

    up

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-#include "binary_trees.h"
-
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-
-	size_t l = 0, r = 0;
-
-	if (!tree)
-		return (0);
-	if (!tree->left && !tree->right)
-		return (1);
-
-	if (tree->left)
-		l = 1;
-	if (tree->right)
-		r = 1;
-
-}
-=======
 #include "binary_trees.h"
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
@@ -34,4 +14,3 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	return (l + r);
 }
->>>>>>> b2f5bc6dd41e9b7b3a880ba250c8a8a97f8e1153
