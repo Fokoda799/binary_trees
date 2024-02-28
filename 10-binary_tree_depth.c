5a3f86287f65a38255fa195a53f4ce699f952dca
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-28 21:23:21 +0100

    task 11

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,9 +4,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL && tree->parent == NULL)
+	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
 	depth = binary_tree_depth(tree->parent);
 
 	return (depth + 1);
+}
