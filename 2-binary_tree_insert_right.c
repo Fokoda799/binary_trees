375b8371507042299e4d2bc697301913a9ffd114
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-28 18:41:50 +0100

    corr2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,11 +13,13 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	binary_tree_t *newNode ;
 	binary_tree_t *tmp;
 
 	if (parent == NULL || newNode == NULL)
 		return (NULL);
+
+	newNode = binary_tree_t * malloc(sizeof(binary_tree_t));
 
 	newNode->n = value;
 	newNode->parent = parent;
