634f2e2cc554f81b2bdb0dfcc72136f5d01e16a0
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-29 09:26:22 -0800

    up

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,16 +6,16 @@
  *
  * Return: a pointer to the sibling node
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	
+
 	binary_tree_t *l_sibling = node->parent->left;
 	binary_tree_t *r_sibling = node->parent->right;
 
 	if (node == l_sibling)
-		return r_sibling
+		return (r_sibling);
 	else
-		return l_sibling
+		return (l_sibling);
 }
