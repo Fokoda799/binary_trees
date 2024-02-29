2d33ee53eb40ce86bff0bda8972f9dec0e23b07f
Fokoda799 <feyt2003@gmail.com>
2024-02-29 20:25:13 +0300

    ok

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,26 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - function that gets the size of a binary tree.
- *
- * @tree: Pointer to the root node
- *
- * Return: 1 or 0.
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t l, r;
-
-	if (!tree)
-		return (0);
-
-	l = binary_tree_size(tree->left);
-	r = binary_tree_size(tree->right);
-
-	return (l + r + 1);
-}
-
-/**
  * binary_tree_height - function that gets the height of a binary tree.
  *
  * @tree: Pointer to the root node
@@ -44,26 +24,45 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect.
+ * binary_tree_balance - measures the balance factor of a binary tree.
  *
- * @tree: Pointer to the root node
+ * @tree: pointer to the root node
  *
- * Return: 1 or 0.
+ * Return: balance value or 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l, r;
+
+	if (!tree)
+		return (0);
+
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+
+	return (l - r);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect.
+ *
+ * @tree: pointer to the root node
+ *
+ * Return: 0 or 1 for perfect
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int l_size, r_size, l_height, r_height;
-
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
-	l_size = (int)binary_tree_size(tree->left);
-	r_size = (int)binary_tree_size(tree->right);
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
+	if (binary_tree_balance(tree) == 0)
+	{
+		if (binary_tree_height(tree->left) == 0 &&
+			binary_tree_height(tree->right) == 0)
+			return (1);
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+	}
 
-	if (l_size == r_size && l_height == r_height)
-		return (1);
-	else
-		return (0);
+	return (0);
 }
