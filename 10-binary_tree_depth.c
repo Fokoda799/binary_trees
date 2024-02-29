4cf276eeda138c57437198b8f382a78f8c02a5c5
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-29 11:27:56 -0800

    up

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_depth - measures the depth of a node in a binary tree
- *@tree: a pointer to the node to measure the depth
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: a pointer to the node to measure the depth
+ *
+ * Return: the depth of the binary tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
