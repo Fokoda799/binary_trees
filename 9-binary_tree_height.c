55ae09a05bec1df78746bc07c8afbc741dcaac0c
fatimaelasri01 <fatimaelassri041@gmail.com>
2024-02-29 11:25:18 -0800

    up

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,9 @@
 
 /**
  * binary_tree_height - measures the height of a binary tree
- *@tree: a pointer to the root node of the tree to measure the heigh
+ * @tree: a pointer to the root node of the tree to measure the heigh
+ *
+ * Return: height of binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
