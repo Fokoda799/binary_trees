5e8f2693f5945c1da3fb8e841755a4d3b30e6c06
Fokoda799 <feyt2003@gmail.com>
2024-03-01 20:47:16 +0300

    103-binary_tree_rotate_left.c

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Move the tree nodes from
+ * right to left.
+ *
+ * @tree: Pointer to the parent node
+ *
+ * Return: New root;
+ */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *newRoot, *tmp;
+
+	if (!tree)
+		return (NULL);
+
+	if (!tree->right)
+		return (tree);
+
+	if (tree->right->left)
+		tmp = tree->right->left;
+
+	newRoot = tree->right;
+	tree->right->parent = NULL;
+	tree->parent = tree->right;
+	tree->right->left = tree;
+	tree->right = tmp;
+
+	if (tmp)
+		tmp->parent = tree;
+
+	return (newRoot);
+}
